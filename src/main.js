2c8ba8c19f5cd4a0868e6aef051c4be93b0f2304
Dominic <dchristinger92@bluewin.ch>
2019-08-27 00:04:32 +0200

    STructure changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import router from './config/router'
+import store from './config/store'
+import './config/registerServiceWorker'
 
 Vue.config.productionTip = false
 
