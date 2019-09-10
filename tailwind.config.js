6db0f3ab5b371c5d5ecf96ccb60bbb19f7649037
Dominic <dchristinger92@bluewin.ch>
2019-09-10 18:12:12 +0200

    STructure changes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2c3e50',
+        active: '#42b983',
+        stv: {
+          default: '#0c6700',
+          tv: 'red',
+          dr: 'blue'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
